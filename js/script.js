1f102886521cf7dd6b2d87c4e98231867d196d5b
Cyprien <lepetit.ct@gmail.com>
2022-06-23 17:37:21 +0200

    main fait; responsive a faire

--- js/script.js
+++ js/script.js
@@ -2,3 +2,32 @@ $(document).ready($('#logo_box').removeClass('d-none'), setTimeout(addDnone, 400
 function addDnone() {
     $('#logo_box').addClass('d-none')
 }
+$(document).ready(setTimeout(addVisible, 5000))
+function addVisible() {
+    $("body").css("overflow", "visible")
+}
+
+const header = $("header");
+const scrollUp = "scroll-up";
+const scrollDown = "scroll-down";
+var lastScroll = 0;
+ 
+window.addEventListener("scroll", () => {
+  const currentScroll = window.pageYOffset;
+  if (currentScroll <= 0) {
+    header.removeClass(scrollUp);
+    return;
+  }
+ 
+  if (currentScroll > lastScroll && !header.hasClass(scrollDown)) {
+    // down
+    header.removeClass(scrollUp);
+    header.addClass(scrollDown);
+  } 
+  else if (currentScroll < lastScroll && header.hasClass(scrollDown)) {
+    // up
+    header.removeClass(scrollDown);
+    header.addClass(scrollUp);
+  }
+  lastScroll = currentScroll;
+});
