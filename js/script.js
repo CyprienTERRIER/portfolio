2d3c2faed182dd31107d45a12356658880ffbf79
Cyprien <lepetit.ct@gmail.com>
2022-06-24 19:12:13 +0200

    A propos + Competence fait

--- js/script.js
+++ js/script.js
@@ -23,7 +23,7 @@ window.addEventListener("scroll", () => {
   lastScroll = currentScroll;
 });
 
-$(document).ready($('#logo_box').removeClass('d-none'), setTimeout(addDnone, 4000), $(this).scrollTop(0), header.removeClass(scrollDown), setTimeout(addVisible, 5000));
+$(document).ready($('#logo_box').removeClass('d-none'), setTimeout(addDnone, 4000), /*$(this).scrollTop(0),*/ header.removeClass(scrollDown), setTimeout(addVisible, 5000));
 
 function addDnone() {
     $('#logo_box').addClass('d-none')
@@ -32,3 +32,21 @@ function addDnone() {
 function addVisible() {
     $('body').css("overflow", "visible")
 };
+
+var divSkills = $('#skills').children()
+for (let index = 0; index < divSkills.length; index++) {
+  console.log(divSkills[index].children[1].textContent)
+  if (divSkills[index].children[1].textContent == 'Débutant') {
+    divSkills[index].children[3].style.animation = '1s ease forwards progressD'
+  }
+  if (divSkills[index].children[1].textContent == 'Intermédiaire') {
+    divSkills[index].children[3].style.animation = '1s ease forwards progressI'
+  }
+  if (divSkills[index].children[1].textContent == 'Avancé') {
+    divSkills[index].children[3].style.animation = '1s ease forwards progressA'
+  }
+  if (divSkills[index].children[1].textContent == 'Expert') {
+    divSkills[index].children[3].style.animation = '1s ease forwards progressE'
+    divSkills[index].children[3].style.borderRadius = '10px'
+  }
+}
